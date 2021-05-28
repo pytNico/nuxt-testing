ef3268d0cdc89e202162b0aefb97c7863b5c3430
pytNico <nc@sippicom.com>
2021-05-28 15:01:29 +0200

    changed router base to work with github pages

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,9 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
-
+  router: {
+    base: '/nuxt-testing/',
+  },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'nuxt-testing',
